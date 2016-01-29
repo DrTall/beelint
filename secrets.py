9c67a077324146e5f2e8718e68c6c8b96b426d17
Sean Fellows <fellows@dropbox.com>
2016-01-29 12:57:47 -0800

    Add new secret.

--- secrets.py
+++ secrets.py
@@ -1,3 +1,13 @@
+# The secrets file provided by Google for use with the Calendar API.
+# Generate one by doing the following:
+# 1. Go to https://console.developers.google.com/project/ and create
+#    a new project.
+# 2. Under APIs & auth -> APIs enable Calendar API.
+# 3. Under APIs & auth -> Credentials create a new client ID for an
+#    installed application. Then click Download JSON
+CLIENT_SECRET_FILE = ''
+
+
 # Your auth token from https://www.beeminder.com/settings/advanced_settings
 # Don't share this with people unless you want them to be able to control
 # your account!
