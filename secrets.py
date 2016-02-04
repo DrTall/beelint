a3873470b935f2cc732a0a8d7f8912aa8a1f0536
Sean <DrTall@users.noreply.github.com>
2016-02-04 08:12:12 -0800

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,10 +1,8 @@
 # The secrets file provided by Google for use with the Calendar API.
-# Generate one by doing the following:
-# 1. Go to https://console.developers.google.com/project/ and create
-#    a new project.
-# 2. Under APIs & auth -> APIs enable Calendar API.
-# 3. Under APIs & auth -> Credentials create a new client ID for an
-#    installed application. Then click Download JSON
+# Generate one by following the steps at
+# https://developers.google.com/google-apps/calendar/quickstart/python
+# You will need to follow all of steps #1 and #2. You can try running
+# the sample provided in #3 to verify, or just try running beelint.
 CLIENT_SECRET_FILE = ''
 
 
